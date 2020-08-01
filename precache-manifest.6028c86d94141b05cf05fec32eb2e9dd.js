67be3b792f5622dc90d7be44005b8b96bf363001
zephinzer <1324745-zephinzer@users.noreply.gitlab.com>
2020-08-01 12:27:15 +0800

    Updates

---
+++ precache-manifest.6028c86d94141b05cf05fec32eb2e9dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f01e4da9cea29b5e0a50edde68ae33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f82e8b866b8451fe31",
+    "url": "/static/css/main.5ba131b3.chunk.css"
+  },
+  {
+    "revision": "1874677cb859965662a9",
+    "url": "/static/js/2.461e0bce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.461e0bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f82e8b866b8451fe31",
+    "url": "/static/js/main.041941d5.chunk.js"
+  },
+  {
+    "revision": "67bf74548da0c598a0c3",
+    "url": "/static/js/runtime-main.e160507c.js"
+  },
+  {
+    "revision": "9fb4384ef820778b4be56ea93cd049c5",
+    "url": "/static/media/logo.9fb4384e.png"
+  }
+]);
